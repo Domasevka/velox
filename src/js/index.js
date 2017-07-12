752c99e1096edce7cc12eb015b2a39b2d41d54d2
Marina Domasevich <m.domasevich@andersenlab.com>
2017-07-12 23:16:11 +0200

    adding footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $( ".main-nav__btn-cross" ).hide();
+  /*  $( ".main-nav__btn-cross" ).hide();
     $( ".main-nav__mobile" ).hide();
     $( ".main-nav__btn-hamburger" ).click(function() {
         $( ".main-nav__mobile" ).slideToggle( "slow", function() {
@@ -14,6 +14,53 @@ $(document).ready(function() {
             $( ".main-nav__btn-cross" ).hide();
             $( ".main-nav__btn-hamburger" ).show();
         });
+    });*/
+
+
+
+    jQuery(function ($) {
+            var slider = $('.mis-stage').miSlider({
+            stageHeight: 380,
+            slidesOnStage: false,
+            slidePosition: 'center',
+            slideStart: 'mid',
+            slideScaling: 150,
+            offsetV: -5,
+            centerV: true,
+            navButtonsOpacity: 1,
+        });
     });
+        var slider = $('.mis-stage').miSlider({
+            speed: 700,
+            pause: 4000,
+            increment: 1,
+            stageHeight: false,
+            slidesOnStage: 1,
+            slidesContinuous: true,
+            slidePosition: "left",
+            slideStart: "beg",
+            slideWidth: false,
+            slideScaling: 100,
+            offsetV: 0,
+            centerV: false,
+            slideClickable: true,
+            navList: true,
+            navButtons: true,
+            navButtonsShow: false,
+            navButtonsOpacity: 0.5,
+            randomize: false,
+            slidesLoaded: false,
+            beforeTrans: false,
+            afterTrans: false,
+            classStage: "mis-stage",
+            classSlider: "mis-slider",
+            classSlide: "mis-slide",
+            classNavButtons: "mis-nav-buttons",
+            classNavList: "mis-nav-list",
+            orSlider: "ol",
+            selectorSlide: "li"
+        });
+
 
 });
+
